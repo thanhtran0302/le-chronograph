98f638fd3c4658f3830a2d5365b2300e1669221b
Thanh TRAN <ttranpro32@gmail.com>
2020-11-30 20:58:38 +0100

    Fix sectionColor

--- src/components/historySection/HistorySection.tsx
+++ src/components/historySection/HistorySection.tsx
@@ -15,7 +15,7 @@ const HistorySection: FC = () => {
       title={t('history')}
     >
       <CommonSection
-        sectionBgColor={'light'}
+        sectionColor={'light'}
         text={`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
     tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
     veniam Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
--- src/components/marketingSection/MarketingSection.tsx
+++ src/components/marketingSection/MarketingSection.tsx
@@ -15,7 +15,7 @@ const MarketingSection: FC = () => {
       title={t('history')}
     >
       <CommonSection
-        sectionBgColor={'dark'}
+        sectionColor={'dark'}
         text={`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
     tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
     veniam Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
